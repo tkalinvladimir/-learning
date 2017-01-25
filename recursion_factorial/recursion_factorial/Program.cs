19658606a048e361f1dfe280f2240921ae67526b
Ткалин Владимир <tkalin.vladimir@gmail.com>
2017-01-25 16:09:15 +1000

    вернул рекурсивную считалку факториала

---
+++ recursion_factorial/recursion_factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            a = fact(a);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+
+        public static int fact(int n)
+        {
+            return (n == 0) ? 1 : n * fact(n - 1);
+        }
+    }
+}
