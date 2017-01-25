84ca07aef09dfb3adaf7a6fda5a0e0f22e03757e
Ткалин Владимир <tkalin.vladimir@gmail.com>
2017-01-25 15:56:17 +1000

    полиморфизм https://tproger.ru/translations/diving-in-oop-p1/ войство одних и тех же объектов и методов принимать разные формы

---
+++ InheritanceAndPolymorphism/InheritanceAndPolymorphism/Overload.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphism
+{
+    public class Overload
+    {
+        private string name = "Vova";
+
+        /*
+        public void Display()
+        {
+            Display2(ref name, ref name);
+            Console.WriteLine(name);
+            DisplayOverload(100, "Vova", "learns", "C#");
+            DisplayOverload(200, "Hello");
+            DisplayOverload(300);
+        }
+        */
+        public static void Display(params object[] objectParamArray)
+        {
+            foreach (object obj in objectParamArray)
+            {
+                Console.Write(obj.GetType().FullName + "   ");
+            }
+        }
+
+
+
+        public void Display2(ref string x, ref string y)
+        {
+            Console.WriteLine(name);
+            x = "Vova 1";
+            Console.WriteLine(name);
+            y = "Vova 2";
+            Console.WriteLine(name);
+            name = "Vova 3";
+            // изменять х и у = изменять name
+        }
+
+
+        //метод идентифицируется не только по имени, но и по его параметрам
+
+        /*
+        public void DisplayOverload(int a) {
+            Console.WriteLine("DisplayOverload " + a);
+        }
+
+        public void DisplayOverload(string a)
+        {
+            Console.WriteLine("DisplayOverload " + a);
+        }
+
+        public void DisplayOverload(string a, int b)
+        {
+            Console.WriteLine("DisplayOverload " + a + b);
+        }
+        */
+        public void DisplayOverload(int a, params string[] parameterArray)
+        {
+            foreach (string str in parameterArray)
+                Console.WriteLine(str + " " + a);
+        }
+    }
+}
---
+++ InheritanceAndPolymorphism/InheritanceAndPolymorphism/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphism
+// наследование и полимофизм
+//метод идентифицируется не только по имени, но и по его параметрам
+// ref - передача и получение по реф - изменение по ссылке
+// передать n параметров - ключевое слово params (последним аргументом)
+// Запомните: массив параметров должен быть одномерным.
+// C# рассматривает методы с массивом параметров последними
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Overload overload = new Overload();
+            /*    overload.DisplayOverload(100);
+                overload.DisplayOverload("method overloading");
+                overload.DisplayOverload("method overloading", 100);
+                */
+
+            object[] objArray = { 100, "Vova", 200.300} ; // массив
+            object obj = objArray; // массив как объект
+
+            Overload.Display(objArray);
+            Console.WriteLine();
+            Overload.Display((object)objArray); // массив приведенный к объекту
+            Console.WriteLine();
+            Overload.Display(obj);
+            Console.WriteLine();
+            Overload.Display((object[])obj); // массив как объект приведенный к массиву 
+
+            Console.ReadKey();
+
+        }
+    }
+}
