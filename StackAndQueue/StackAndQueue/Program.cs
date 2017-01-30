9a7b1902167b13b391b09b9b5ce9ad1687d7afa1
Ткалин Владимир <tkalin.vladimir@gmail.com>
2017-01-30 16:05:39 +1000

    стек и очередь

---
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    //Стек — это коллекция, элементы которой получают по принципу «последний вошел, первый вышел» (Last-In-First-Out или LIFO). Это значит, что мы будем иметь доступ только к последнему добавленному элементу.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack();
+            Console.ReadKey();
+            Queue();
+            Console.ReadKey();
+        }
+
+        private static void Dequeue()
+        {
+            Queue<string> numbers = new Queue<string>();
+            
+            numbers.Enqueue("one");
+            numbers.Enqueue("two");
+            numbers.Enqueue("three");
+            numbers.Enqueue("four");
+            numbers.Enqueue("five");
+            numbers.Dequeue();
+            // A queue can be enumerated without disturbing its contents.
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("\nDequeuing '{0}'", numbers.Dequeue());
+            Console.WriteLine("Peek at next item to dequeue: {0}",
+                numbers.Peek());
+            Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
+
+            // Create a copy of the queue, using the ToArray method and the
+            // constructor that accepts an IEnumerable<T>.
+            Queue<string> queueCopy = new Queue<string>(numbers.ToArray());
+
+            Console.WriteLine("\nContents of the first copy:");
+            foreach (string number in queueCopy)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Create an array twice the size of the queue and copy the
+            // elements of the queue, starting at the middle of the 
+            // array. 
+            string[] array2 = new string[numbers.Count * 2];
+            numbers.CopyTo(array2, numbers.Count);
+
+            // Create a second queue, using the constructor that accepts an
+            // IEnumerable(Of T).
+            Queue<string> queueCopy2 = new Queue<string>(array2);
+
+            Console.WriteLine("\nContents of the second copy, with duplicates and nulls:");
+            foreach (string number in queueCopy2)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("\nqueueCopy.Contains(\"four\") = {0}",
+                queueCopy.Contains("four"));
+
+            Console.WriteLine("\nqueueCopy.Clear()");
+            queueCopy.Clear();
+            Console.WriteLine("\nqueueCopy.Count = {0}", queueCopy.Count);
+        }
+
+        private static void Queue()
+        {
+            Queue<string> numbers = new Queue<string>();
+            numbers.Enqueue("one");
+            numbers.Enqueue("two");
+            numbers.Enqueue("three");
+            numbers.Enqueue("four");
+            numbers.Enqueue("five");
+
+            // A queue can be enumerated without disturbing its contents.
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("\nDequeuing '{0}'", numbers.Dequeue());
+            Console.WriteLine("Peek at next item to dequeue: {0}",
+                numbers.Peek());
+            Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
+
+            // Create a copy of the queue, using the ToArray method and the
+            // constructor that accepts an IEnumerable<T>.
+            Queue<string> queueCopy = new Queue<string>(numbers.ToArray());
+
+            Console.WriteLine("\nContents of the first copy:");
+            foreach (string number in queueCopy)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Create an array twice the size of the queue and copy the
+            // elements of the queue, starting at the middle of the 
+            // array. 
+            string[] array2 = new string[numbers.Count * 2];
+            numbers.CopyTo(array2, numbers.Count);
+
+            // Create a second queue, using the constructor that accepts an
+            // IEnumerable(Of T).
+            Queue<string> queueCopy2 = new Queue<string>(array2);
+
+            Console.WriteLine("\nContents of the second copy, with duplicates and nulls:");
+            foreach (string number in queueCopy2)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("\nqueueCopy.Contains(\"four\") = {0}",
+                queueCopy.Contains("four"));
+
+            Console.WriteLine("\nqueueCopy.Clear()");
+            queueCopy.Clear();
+            Console.WriteLine("\nqueueCopy.Count = {0}", queueCopy.Count);
+        }
+
+        private static void Stack()
+        {
+            Stack<string> numbers = new Stack<string>();
+            numbers.Push("one");
+            numbers.Push("two");
+            numbers.Push("three");
+            numbers.Push("four");
+            numbers.Push("five");
+            Console.SetWindowSize(150, 50);
+            // A stack can be enumerated without disturbing its contents.
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("\nPopping '{0}'", numbers.Pop());
+            Console.WriteLine("Peek at next item to destack: {0}",
+                numbers.Peek());
+            Console.WriteLine("Popping '{0}'", numbers.Pop());
+
+            // Create a copy of the stack, using the ToArray method and the
+            // constructor that accepts an IEnumerable<T>.
+            Stack<string> stack2 = new Stack<string>(numbers.ToArray());
+
+            Console.WriteLine("\nContents of the first copy:");
+            foreach (string number in stack2)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Create an array twice the size of the stack and copy the
+            // elements of the stack, starting at the middle of the 
+            // array. 
+            string[] array2 = new string[numbers.Count * 2];
+            numbers.CopyTo(array2, numbers.Count);
+
+            // Create a second stack, using the constructor that accepts an
+            // IEnumerable(Of T).
+            Stack<string> stack3 = new Stack<string>(array2);
+
+            Console.WriteLine("\nContents of the second copy, with duplicates and nulls:");
+            foreach (string number in stack3)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("\nstack2.Contains(\"four\") = {0}",
+                stack2.Contains("four"));
+
+            Console.WriteLine("\nstack2.Clear()");
+            stack2.Clear();
+            Console.WriteLine("\nstack2.Count = {0}", stack2.Count);
+        }
+    }
+}
