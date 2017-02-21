31431aba4ff9b91686861e72bef5f0db81989e51
Ткалин Владимир <tkalin.vladimir@gmail.com>
2017-02-21 13:48:36 +1000

    learning enum

---
+++ enum_test/enum_test/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_test
+{
+    /*Создайте класс Кошка. У кошки будет свойство «уровень сытости» и метод «съесть что-то». 
+     * Создайте перечисление Еда (рыба, мышь…). Каждый вид еды должен по-разному изменять уровень сытости.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.prnt();
+            cat.EatSmth(Food.fish);
+            cat.prnt();
+            cat.EatSmth(Food.mouse);
+            cat.prnt();
+            Console.ReadKey();
+
+        }
+    }
+
+
+    enum Food : int { mouse = 50 , fish = 20 };
+
+    class Cat
+    {
+        private int satiety;
+
+        public void EatSmth(Food smth)
+        {
+            this.satiety = this.satiety + (int)smth;
+        }
+
+        public Cat()
+        {
+            satiety = 0;  
+        }
+
+        public void prnt()
+        {
+            Console.WriteLine(this.satiety);
+        }
+    }
+
+}
---
+++ enum_test/enum_test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("enum_test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("enum_test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e6db1039-2a19-4385-8fca-a18342a945b0")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
