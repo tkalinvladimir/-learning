730075f28dd7364651f19599047da9effee1836d
Ткалин Владимир <tkalin.vladimir@gmail.com>
2017-02-22 15:18:48 +1000

    Структуры и поиск ближайшего к среднему http://mycsharp.ru/post/48/2014_12_21_struktury_v_si-sharp.html

---
+++ structures/structures/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structures
+{
+    /*
+    Создайте программу, которая будет находить окружность (структура) у которой радиус максимально близкий к среднему значению радиусов окружностей из списка.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] radius = new double[] { 2.3, 2.1, 2,7, 2.9, 1.8 };
+            double averageBYlinq = (from ord in radius select ord).Average();
+            Console.WriteLine("The average is " + averageBYlinq);
+            Circle c;
+            c.calcradiuses(radius);
+            Console.ReadKey();
+
+        }
+     }
+
+    struct Circle
+    {
+        public void calcradiuses(double[] radiuses)
+        {
+            double averageRad = radiuses.Average();
+            double nearest = 0;
+            double find = radiuses.Max();
+
+            foreach (double rad in radiuses)
+            {
+                if (Math.Abs(averageRad - rad) < find)
+                {
+                    nearest = rad;
+                    find = Math.Abs(averageRad - nearest);
+                }
+            }
+            // пробегаем массив и находим элемент с минимальным значением |ai-ac|, где ai - i-й элемент массива, ac - среднее значение. 
+
+            Console.WriteLine("The nearest member of the array to the average array is " + nearest);
+            double circumference = nearest * 2 * Math.PI;
+            Console.WriteLine("The circumference (C=2*R*Pi) is " + circumference);
+
+        }
+
+    }
+}
---
+++ structures/structures/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("structures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("structures")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7a8e77a6-59a1-447c-9c1d-00be5ea2121f")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
