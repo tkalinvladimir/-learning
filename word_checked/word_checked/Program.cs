160cc9a9c220b92277fa0209fbe356ea17ce2389
Ткалин Владимир <tkalin.vladimir@gmail.com>
2017-03-01 10:22:31 +1000

    При использовании ключевого слова checked приложение выбрасывает исключение о переполнении. Поэтому для его обработки в данном случае используется конструкция try...catch. Подробнее данную конструкцию и обработку исключений мы рассмотрим позже, а пока надо знать, что в блок try мы включаем действия, в которых может потенциально возникнуть ошибка, а в блоке catch обрабатываем ошибку.

---
+++ word_checked/word_checked/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace word_checked
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            try
+            {
+                int a = 33;
+                int b = 600;
+                byte c = checked((byte)(a + b));
+                Console.WriteLine(c);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
