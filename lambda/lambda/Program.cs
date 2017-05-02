bfbcc7d71c50ebf77f6c92c733efd55147eb6d27
Ткалин Владимир <tkalin.vladimir@gmail.com>
2017-05-02 14:10:35 +1000

    лямбды

---
+++ lambda/lambda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda
+{
+    class Program
+    {
+        delegate int Z(int a, int b);
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Z res = (a,b) =>  a * b; 
+            Console.WriteLine(res(x,y));
+            Console.ReadKey();
+        }
+    }
+}
